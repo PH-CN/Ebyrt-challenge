91e41e6c3b9002ac6d67d444654366acbcd78b56
Pedro Candido <pedrohcn02@gmail.com>
2022-07-01 16:46:58 -0300

    missing createdat and updatedat in tasks table fix

--- Back-end/src/migrations/20220701193457-create-tasks-table.js
+++ Back-end/src/migrations/20220701193457-create-tasks-table.js
@@ -16,6 +16,27 @@ module.exports = {
 			status: {
 				allowNull: false,
 				type: Sequelize.STRING
+			},
+			userId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				onUpdate: 'CASCADE',
+				onDelete: 'CASCADE',
+				field: 'user_id',
+				references: {
+					model: 'Users',
+					key: 'id'
+				},
+				createdAt: {
+					allowNull: false,
+					field: 'created_at',
+					type: Sequelize.DATE
+				},
+				updatedAt: {
+					allowNull: false,
+					field: 'updated_at',
+					type: Sequelize.DATE
+				}
 			}
 		});
 	},
