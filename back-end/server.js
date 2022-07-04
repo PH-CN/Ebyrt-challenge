f11a4237fdf3f1fd3c2f319729d916eed7f7213b
Pedro Candido <pedrohcn02@gmail.com>
2022-07-04 13:44:32 -0300

    adding use of express.json on back end

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,11 @@
 const app = require('./app');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require('express');
 const userController = require('./src/controllers/userController');
 
 const port = (process.env.PORT || 3000);
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
